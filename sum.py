bc0fb592855d95ce7eb1857b561828478269fd7b
Cheung <31495117+cheung1@users.noreply.github.com>
2017-10-28 08:37:52 +0800

    Create sum.py

---
+++ sum.py
@@ -0,0 +1,11 @@
+#define a function to calculate 2 number add results
+def add2num():
+	i = input('please input one number : ')
+	j = input('please input another number : ')
+	sum = i+j
+	print 'sum=%d'%sum
+
+
+
+
+add2num()
