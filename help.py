f2bfd7693bedab38f8ed982bb31847484bb9844a
Cheung <31495117+cheung1@users.noreply.github.com>
2017-10-28 08:34:52 +0800

    Create help.py

---
+++ help.py
@@ -0,0 +1,21 @@
+
+#1.write the triangle print code
+# notice: when you just define, it cannot execute
+
+#2.define a function
+
+def printTriangle():
+	'this is the help document '
+	print '  *  '
+	print ' * * '
+	print '* * *'
+
+
+#3.invoke the function
+#notice:only can via the invoke to use the function
+# invoke the function: write the function name,then add a pair of bracket
+printTriangle()
+
+#notice:define only once,but can invoke infite times
+printTriangle()
+
