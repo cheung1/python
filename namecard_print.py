c0788066163858ec24b7c8c9dafbd3e331580bd9
Cheung <31495117+cheung1@users.noreply.github.com>
2017-10-28 08:37:00 +0800

    Create namecard_print.py

---
+++ namecard_print.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+#-*- encoding=utf-8
+name = raw_input('请输入您的名字:')
+QQ = raw_input('请输入您的QQ:')
+phone = raw_input('请输入您的手机号:')
+address = raw_input('请输入您的地址:')
+print('正在生成您的名片...\n正在打印您的名片')
+print('=============================\n')
+print('姓名:%s'%name)
+print('QQ:%s'%QQ)
+print('手机号:%s'%phone)
+print('地址:%s\n'%address)
+print('=============================\n')
