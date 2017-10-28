bbd8afbefe4588a0890d71597dceaed6551cd630
Cheung <31495117+cheung1@users.noreply.github.com>
2017-10-28 08:36:04 +0800

    Create name.py

---
+++ name.py
@@ -0,0 +1,21 @@
+#-*-encoding=utf-8
+name='baoqiang'
+
+for x in name:
+	print '---------' 
+	if x == 'i':
+		continue
+	print x
+	print x
+	print x
+	print x
+	print x
+	print x
+	print x
+	print x
+
+
+
+
+
+print '执行结束'
