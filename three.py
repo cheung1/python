10cd78be70031de6d6c2269f2e58f3f1b2119cc7
Cheung <31495117+cheung1@users.noreply.github.com>
2017-10-28 14:19:23 +0800

    Create three.py

---
+++ three.py
@@ -0,0 +1,16 @@
+#pack up
+def add3num(a,b,c):
+	sum = a+b+c
+	#print '%d'%sum
+	return sum # return is to return the results to another one
+
+
+
+result = add3num(11,22,33) # return has a return value,no return means no value to return
+#in the return position, need a parameter to accept it
+
+print '************************%d**********************'%result
+
+
+
+
