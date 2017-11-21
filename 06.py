3292124962a0619cfc292252c158e76465c5f7a2
Cheung <31495117+cheung1@users.noreply.github.com>
2017-11-21 20:04:19 +0800

    Create 06.py

---
+++ 06.py
@@ -0,0 +1,13 @@
+class Tool(object):
+    
+    #config
+    num = 0
+
+    #method
+    def __init__(self,new_name):
+        self.name = new_name
+
+
+tool1 = Tool('spade')
+tool2 = Tool('shave')
+tool3 = Tool('bucket')
