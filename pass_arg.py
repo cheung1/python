433af42dd3eea740ef7039d7ffc888939bda54cb
Cheung <31495117+cheung1@users.noreply.github.com>
2018-01-30 18:00:13 +0800

    Create pass_arg.py

---
+++ pass_arg.py
@@ -0,0 +1,13 @@
+import sys
+
+print(sys.argv)
+
+name = sys.argv[1]
+
+print("welecome %s coming"%name)
+
+# index=0 is the title
+
+>>python3 04-给程序传参.py laoli
+>>['04-给程序传参.py', 'laoli']
+>>welecome laoli coming
